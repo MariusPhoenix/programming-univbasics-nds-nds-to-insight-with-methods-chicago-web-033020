551f338e9804f3a57cd1993e9927968003c07673
Marius Phoenix <phoenixzenomarius@gmail.com>
2020-03-24 03:30:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 #binding.pry
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
